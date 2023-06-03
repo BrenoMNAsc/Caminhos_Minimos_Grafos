cfd81083a31fba514e9ad8880b7bc5255f2ebdec
breno <breno.golo@gmail.com>
2023-06-03 10:17:10 -0300

    terminando Dijkstra

--- Dijkstra/main.py
+++ Dijkstra/main.py
@@ -1,16 +1,38 @@
-from Grafo import Grafo
+import heapq
+import math
 
-grafo = Grafo(11)
-grafo.adicionar_aresta(0, 1, 4)
-grafo.adicionar_aresta(0, 7, 8)
-grafo.adicionar_aresta(1, 2, 8)
-grafo.adicionar_aresta(1, 7, 11)
-grafo.adicionar_aresta(2, 3, 7)
-grafo.adicionar_aresta(2, 5, 4)
-grafo.adicionar_aresta(6, 7, 1)
-grafo.adicionar_aresta(6, 8, 6)
-grafo.adicionar_aresta(7, 8, 7)
-grafo.adicionar_aresta(9, 0, 2)
-grafo.adicionar_aresta(10, 0, 4)
+def main():
+    grafo = {1: {1: 0, 2: 10, 3: 5, 4: math.inf, 5: math.inf},
+             2: {1: math.inf, 2: 0, 3: 2, 4: -7, 5: math.inf},
+             3: {1: math.inf, 2: 3, 3: 0, 4: 9, 5: -2},
+             4: {1: math.inf, 2: math.inf, 3: math.inf, 4: 0, 5: 4},
+             5: {1: 7, 2: math.inf, 3: math.inf, 4: 6, 5: 0}}
+    c = Dijkstra(grafo, 1)
+    exibir_matriz_caminhos(c)
 
-grafo.dijkstra(0)
+
+def Dijkstra(grafo, v):
+    c = []
+    for i in range(2, len(grafo) + 1):
+        c.append(grafo[v][i])
+    c[v] = 0
+    heap = []
+    heapq.heappush(heap, (0 ,v))
+    while heap:
+        peso, vertice = heapq.heappop(heap)
+        for vizinho, pesoAresta in grafo[vertice].items():
+            novoPeso = c[vertice] + pesoAresta
+            if novoPeso < c[vizinho]:
+                c[vizinho] = novoPeso
+                heapq.heappush(heap, (novoPeso, vizinho))
+
+    return c
+
+def exibir_matriz_caminhos(c):
+    for l in sorted(c.keys()):
+        for v in sorted(c[l].keys()):
+            print(f"{c[l][v]:5}", end=" ")
+        print()
+
+if __name__ == "__main__":
+    main()
